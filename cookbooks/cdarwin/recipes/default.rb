4c490cd12d06c06ad5ecbb77f1cb4fa74bd1c479
Jason Thigpen <darwin@senet.us>
2012-02-15 12:37:42 -0800

    initial role gets my preferred user on the system with my shell and my dot files

---
+++ cookbooks/cdarwin/recipes/default.rb
@@ -0,0 +1,69 @@
+#
+# Cookbook Name:: cdarwin
+# Recipe:: default
+#
+# Copyright 2012, Jason Thigpen
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+%w{git-core tmux vim pdksh}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+search(:users, 'id:cdarwin') do |u|
+  home_dir = "/home/#{u['id']}"
+
+  template "#{home_dir}/.ssh/id_rsa" do
+    source "id_rsa.erb"
+    owner  u['id']
+    group  u['id']
+    mode   0600
+    variables :priv_key => u['priv_key']
+  end
+
+  template "#{home_dir}/.ssh/known_hosts" do
+    source "known_hosts.erb"
+    owner  u['id']
+    group  u['id']
+    mode   0644
+    variables :known_hosts => u['known_hosts']
+  end
+
+  execute "put_dot_files_in_place" do
+    cwd         "#{home_dir}/dot"
+    user        u['id']
+    group       u['id']
+    environment ({'HOME' => "#{home_dir}"})
+    command     "for i in .[a-zA-Z]* ; do [ ${i} != '.git' ] && cp -r ${i} ~/ ; done"
+    action      :nothing
+  end
+
+  git "#{home_dir}/dot" do
+    repository "git@github.com:cdarwin/dot.git"
+    reference  "master"
+    user       u['id']
+    group      u['id']
+    action     :sync
+    notifies   :run, resources(:execute => "put_dot_files_in_place")
+  end
+end
---
+++ roles/cdarwin.rb
@@ -0,0 +1,20 @@
+name "cdarwin"
+description "Setup cdarwin user and configure sudo and stuff."
+# List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
+run_list(
+    "recipe[users::sysadmins]",
+    "recipe[sudo]",
+    "recipe[cdarwin]"
+)
+# Attributes applied if the node doesn't have it set already.
+#default_attributes()
+# Attributes applied no matter what the node has set already.
+override_attributes(
+    :authorization => {
+        :sudo => {
+            :groups => ["sudo", "admin", "sysadmin"],
+            :users => ["cdarwin"],
+            :passwordless => true
+        }
+    }
+)
