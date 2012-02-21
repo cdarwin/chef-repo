a280e7cf30e504c0b3d21b63ecaaa84dbe64a516
Jason Thigpen <darwin@senet.us>
2012-02-20 19:24:30 -0800

    initial cookbook to install 9base

---
+++ cookbooks/9base/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Jason Thigpen"
+maintainer_email "darwin@senet.us"
+license          "MIT"
+description      "Installs/Configures 9base"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
